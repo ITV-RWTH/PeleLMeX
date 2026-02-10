50ddb9fe0ba21466bfbb5899e992183e9f3b6ac9
Justin Freiberger <justin.freiberger@rwth-aachen.de>
2026-02-10 13:57:51 +0100

    initial fix

--- Source/PeleLMeX_Plot.cpp
+++ Source/PeleLMeX_Plot.cpp
@@ -1179,11 +1179,11 @@ PeleLM::addLevelVelocityDataFromPlt(int a_lev, const std::string& a_velPltFile)
 
   // load data from plot file
   amrex::BoxArray tmpVelBA(ldata_p->state.boxArray());
-  amrex::DistributionMapping tmpVelDM(tmpVelBA);
+  amrex::DistributionMapping tmpVelDM(ldata_p->state.DistributionMap());
   int nGrow0(0), sComp0(0);
   amrex::MultiFab tmpVel(tmpVelBA, tmpVelDM, AMREX_SPACEDIM, nGrow0);
   pltData.fillPatchFromPlt(
-    a_lev, geom[a_lev], idXvel, sComp0, AMREX_SPACEDIM, tmpVel);
+    a_lev, geom[a_lev], idXvel, sComp0, AMREX_SPACEDIM, tmpVel, 0);
   // scale the velocity
   tmpVel.mult(m_velocity_plotfile_scale);
   amrex::MultiFab::Add(ldata_p->state, tmpVel, 0, VELX, AMREX_SPACEDIM, 0);
